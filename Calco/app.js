803be2bb024dc4f7065dbde46c52fdafdb30b796
ohchloeho <95035928+ohchloeho@users.noreply.github.com>
2022-09-09 15:02:45 +0800

    ➕Calco Update included switch statement and eval method

--- Calco/app.js
+++ Calco/app.js
@@ -1,1 +1,34 @@
-console.log('this is calco the calculator');
+console.log("this is calco the calculator");
+//output values
+const output = document.querySelector(".output > p");
+
+const updateOutput = (outputValue) => {
+  output.textContent = outputValue;
+};
+
+let value = "";
+
+//button numbers
+const inputBtns = document.querySelectorAll("button");
+inputBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    //use switch case properly
+    switch (btn.textContent) {
+      case "AC":
+        value = "";
+        break;
+      case "=":
+        value = eval(value);
+        break;
+      case "x":
+        value = "*";
+        break;
+      case "÷":
+        value = "/";
+        break;
+      default:
+        value += btn.textContent;
+    }
+    updateOutput(value);
+  });
+});
